3da08ccf43edcbd156459aaf1ed8be9be1518e79
Wasthu-T <110516139+Wasthu-T@users.noreply.github.com>
2025-01-24 15:13:03 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,6 @@
 from flask import Flask
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 
 # Inisiasi Flask
 app = Flask(__name__)
-limiter = Limiter(
-    key_func=get_remote_address,
-    app=app,
-    default_limits=["200 per day", "50 per hour"]
-)
 
 from app import routes
