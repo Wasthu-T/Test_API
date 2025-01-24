39da686470d2b508d02ca366b8bfd9ef7654ac1c
Wasthu-T <110516139+Wasthu-T@users.noreply.github.com>
2025-01-24 15:13:16 +0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,8 @@
-from app import app, response, limiter
+from app import app, response
 from app.controller import aicontroller
 from flask import request
 
 @app.route('/api/predict', methods=["POST"])
-@limiter.limit("10 per minute")
 def predict() : 
     if request.method == 'POST':
         return aicontroller.predict()
